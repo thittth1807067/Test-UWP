f77a43f762e9ec6f29a400b025f3ed0bebc40c47
unknown <thittth1807067@fpt.edu.vn>
2019-11-06 16:13:20 +0700

    test-UWP

---
+++ Test-UWP/ContactModel.cs
@@ -0,0 +1,85 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_UWP.entity;
+using Test_UWP.Utils;
+
+namespace Test_UWP
+{
+    class ContactModel
+    {
+        public bool Insert(Contact contact )
+        {
+            try
+            {
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("INSERT INTO Contact (Name, Phone) VALUES (?, ?)"))
+                {
+                    stt.Bind(1, contact.Name);
+                    stt.Bind(2, contact.Phone);
+                  
+                    stt.Step();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false;
+        }
+        public Contact GetContactByName(string name)
+        {
+            try
+            {
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("SELECT * FROM Contact WHERE Name = ?"))
+                {
+                    stt.Bind(1, name);
+                    if (stt.Step() == SQLiteResult.ROW)
+                    {
+                        return (new Contact
+                        {
+                            Name = (string)stt[1],
+                            Phone = (long)stt[2],
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        public ObservableCollection<Contact> GetListContacts()
+        {
+            try
+            {
+                var list = new ObservableCollection<Contact>();
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("SELECT * FROM Contact"))
+                {
+
+                    while (stt.Step() == SQLiteResult.ROW)
+                    {
+                        list.Add(new Contact
+                        {
+                            Name = (string)stt[1],
+                            Phone = (long)stt[2],
+                        });
+                    }
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ Test-UWP/Utils/SQLiteUtil.cs
@@ -0,0 +1,41 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_UWP.Utils
+{
+    class SQLiteUtil
+    {
+         private const string DatabaseName = "demo.db";
+
+        private static SQLiteUtil _instance;
+        public SQLiteConnection Connection { get; }
+
+        public static SQLiteUtil GetIntances()
+        {
+            if (_instance == null)
+            {
+                _instance = new SQLiteUtil();
+            }
+            return _instance;
+        }
+
+        private SQLiteUtil()
+        {
+            Connection = new SQLiteConnection(DatabaseName);
+            CreateTables();
+        }
+
+        private void CreateTables()
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS Contact (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Name VARCHAR( 140 ) NOT NULL, PhoneNumber UNSIGNED BIG INT NOT NULL UNIQUE);";
+            using (var statement = Connection.Prepare(sql))
+            {
+                statement.Step();
+            }
+        }
+    }
+}
